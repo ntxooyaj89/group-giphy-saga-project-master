a7313fec8a315e861f53fa69d9bc660ad916a29c
mes32 <stockman.mike@gmail.com>
2019-01-31 16:09:58 -0600

    Add component FavoriteGifs

---
+++ src/components/FavoriteGifs/FavoriteGifs.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import Header from '../Header/Header.js';
+
+class FavoriteGifs extends Component {
+    render() {
+        return (
+            <Header />
+            
+        );
+    }
+}
+
+export default FavoriteGifs;
