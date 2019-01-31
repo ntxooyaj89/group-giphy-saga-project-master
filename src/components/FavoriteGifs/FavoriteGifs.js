5b7d332a46abf8179fc6690a9772d48118a8e1c4
mes32 <stockman.mike@gmail.com>
2019-01-31 16:47:14 -0600

    Build FavoriteGif component.

--- src/components/FavoriteGifs/FavoriteGifs.js
+++ src/components/FavoriteGifs/FavoriteGifs.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 
 import Header from '../Header/Header.js';
+//import FavoriteCard from '../FavoriteCard/FavoriteCard.js'; 
 
 class FavoriteGifs extends Component {
     render() {
         return (
-            <Header />
-            {this.props.rs.}
+            <div>
+                <Header />
+                {/* { this.props.reduxStore.favoritesReducer.map((favorite) => {
+                        return <FavoriteCard key={favorite.id} favorite={favorite} />
+                    });
+                } */}
+            </div>
         );
     }
 }
 
-const mapReduxStoreToProps = (reduxStore) => { 
-    reduxStore: reduxStore,
-};
+const mapReduxStoreToProps = (reduxStore) => ({ 
+    reduxStore
+});
 export default connect(mapReduxStoreToProps)(FavoriteGifs);
