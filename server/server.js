2c9cbf6da520a0ecc3dee9b43455d5da14a6ab97
Tang Yang <ty4081097@gmail.com>
2019-01-31 11:46:48 -0600

    added github url.

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Route includes
+const favoriteRouter = require('./routes/favorite.router');
+const categoryRouter = require('./routes/category.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('build'));
+
+/* Routes */
+app.use('/api/favorite', favoriteRouter);
+app.use('/api/category', categoryRouter);
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Giphy Search!</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
