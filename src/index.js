1cb20d94b294ea56663938708b765c251d56a93a
Tang Yang <ty4081097@gmail.com>
2019-01-31 11:58:36 -0600

    modify index.js file.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+function* rootSaga(){
+    
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+        plantList,
+        boxList
+    }),
+    // Add sagaMiddleware to our store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+
+
+// ReactDOM.render(<App />, document.getElementById('react-root'));
