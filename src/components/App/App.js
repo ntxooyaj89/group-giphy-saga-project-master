2ff77a038cfe5566d6c6a5c32e8c756c0f84afa2
mes32 <stockman.mike@gmail.com>
2019-01-31 17:54:10 -0600

    Setup router in 'App.js'.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import Header from './../Header/Header.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import FavoriteGifs from '../FavoriteGifs/FavoriteGifs.js';
 import SearchGif from './../SearchGif/SearchGif.js';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header className='App-header' title={'title'}/>
-        <SearchGif />
+      <div className="App">
+        <Router>
+          <div>
+            <Route exact path="/" component={SearchGif} />
+            <Route exact path="/search" component={SearchGif} />
+            <Route exact path="/favorite" component={FavoriteGifs} />
+          </div>
+        </Router>
       </div>
     );
   }
