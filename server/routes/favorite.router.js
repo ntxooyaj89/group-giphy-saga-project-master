c09df84732ec1ac26be54fb42e6526d9c8ba4c40
Cody Troop <codytroop@yahoo.com>
2019-01-31 15:52:20 -0600

    get /api/favorite returns an array of row objects

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,13 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  console.log('GET /api/favorite');
+  pool.query('SELECT * from "favorites";').then((result) => {
+      res.send(result.rows);
+  }).catch((error) => {
+      console.log('Error GET /api/favorites', error)
+      res.sendStatus(500);
+  });
 });
 
 // add a new favorite 
